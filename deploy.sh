c9a570ffd8da3675d97df874d4934c395441f451
Ilgar Gurbanov <ilgar.gurbanov.90@gmail.com>
2026-04-28 23:27:06 +0300

    changed deploy script for AWS EC2 instance-2

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ REMOTE_HOST="18.203.237.117"
 REMOTE_DIR="/home/ubuntu/mysite"
 
 #rsync automation
-/usr/bin/rsync -avz --delete $LOCAL_DIR $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR
+/usr/bin/rsync -avz --delete -e "ssh -i ~/.ssh/ilgar.pem" $LOCAL_DIR $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR
